8c963a65a8b48ffceef6db32dc9eae38fdab49c0
Panda <Panda@gmail.com>
2023-02-14 17:06:44 +0200

    Retrieving SMS messages constently after delayed Time

--- app/src/main/java/com/example/allsafe/MainActivity.java
+++ app/src/main/java/com/example/allsafe/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.TextView;
 import android.Manifest;
@@ -14,6 +15,7 @@ import android.Manifest;
 public class MainActivity extends AppCompatActivity {
 
     private TextView myTextView;
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,19 +27,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void READ_SMS(View view){
-
-        try (Cursor cursor = getContentResolver().query(Uri.parse("content://sms"), null, null, null, null)){
-            StringBuilder smsBuilder = new StringBuilder();
-            if (cursor != null && cursor.moveToFirst()) {
-                do {
-                    String message = cursor.getString(12);
-                    smsBuilder.append(message);
-                    smsBuilder.append("\n");
-                } while (cursor.moveToNext());
-            }
-            myTextView.setText(smsBuilder.toString());
-        }
+        handler.postDelayed(Retrieve_SMS, 5000);
 
     }
+
+    private Runnable Retrieve_SMS = new Runnable() {
+        @Override
+        public void run() {
+            try (Cursor cursor = getContentResolver().query(Uri.parse("content://sms"), null, null, null, null)){
+                StringBuilder smsBuilder = new StringBuilder();
+                if (cursor != null && cursor.moveToFirst()) {
+                    do {
+                        String message = cursor.getString(12);
+                        smsBuilder.append(message);
+                        smsBuilder.append("\n");
+                    } while (cursor.moveToNext());
+                }
+                myTextView.setText(smsBuilder.toString());
+            }
+            handler.postDelayed(this, 5000);
+        }
+    };
 
 }
