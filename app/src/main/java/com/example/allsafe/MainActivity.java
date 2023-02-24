7230c1995e00fbfdf3046a2be38ed4b347884f00
Panda <Panda@gmail.com>
2023-02-25 01:27:10 +0200

    Added Next and Prev buttons to navigate between messages

--- app/src/main/java/com/example/allsafe/MainActivity.java
+++ app/src/main/java/com/example/allsafe/MainActivity.java
@@ -9,13 +9,17 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.Manifest;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView myTextView;
+    private Button nextButton, prevButton;
     private Handler handler = new Handler();
+    private Cursor cursor;
+    private int messageIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,44 +27,70 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         myTextView = findViewById(R.id.textView);
+        nextButton = findViewById(R.id.nextButton);
+        prevButton = findViewById(R.id.prevButton);
+
+
         ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_SMS}, PackageManager.PERMISSION_GRANTED);
 
         READ_SMS();
     }
 
-
     public void READ_SMS(){
-        handler.postDelayed(Retrieve_SMS, 1);
+
+        cursor = getContentResolver().query(Uri.parse("content://sms"), null, null, null, "date DESC");
+
+        if (cursor != null && cursor.moveToFirst()) {
+            messageIndex = 0;
+            displayMessage();
+        }
+
     }
 
-    private Runnable Retrieve_SMS = new Runnable() {
-        @Override
-        public void run() {
-            try (Cursor cursor = getContentResolver().query(Uri.parse("content://sms"), null, null, null, null)){
-                StringBuilder smsBuilder = new StringBuilder();
-                if (cursor != null && cursor.moveToFirst()) {
-                    int addressIndex = cursor.getColumnIndex("address");
-                    if(addressIndex >= 0){
-                        do {
-                            String address = cursor.getString(addressIndex);
-                            String message = cursor.getString(12);
+    public void nextMessage(View view) {
+        if (cursor != null && cursor.moveToNext() && messageIndex < cursor.getCount() - 1) {
 
-                            smsBuilder.append("From: ");
-                            smsBuilder.append(address);
-                            smsBuilder.append("\n");
-                            smsBuilder.append("Message:\n");
-                            smsBuilder.append(message);
-                            smsBuilder.append("\n");
-                            smsBuilder.append("______________________________________");
-                            smsBuilder.append("\n \n");
-
-                        } while (cursor.moveToNext());
-                    }
-                }
-                myTextView.setText(smsBuilder.toString());
-            }
-            handler.postDelayed(this, 5000);
+            messageIndex++;
+            displayMessage();
         }
-    };
+    }
 
+    public void prevMessage(View view) {
+        if (cursor != null && cursor.moveToPrevious() && messageIndex > 0) {
+            messageIndex--;
+            displayMessage();
+        }
+    }
+
+    private void displayMessage() {
+        if (cursor != null){
+            String address = cursor.getString(cursor.getColumnIndexOrThrow("address"));
+            String message = cursor.getString(cursor.getColumnIndexOrThrow("body"));
+
+            StringBuilder smsBuilder = new StringBuilder();
+
+            smsBuilder.append("From: ");
+            smsBuilder.append(address);
+            smsBuilder.append("\n");
+            smsBuilder.append("Message:\n");
+            smsBuilder.append(message);
+            smsBuilder.append("\n");
+            smsBuilder.append("______________________________________");
+            smsBuilder.append("\n \n");
+
+            myTextView.setText(smsBuilder.toString());
+
+            if (messageIndex == cursor.getCount() - 1) {
+                nextButton.setEnabled(false);
+            } else {
+                nextButton.setEnabled(true);
+            }
+
+            if (messageIndex == 0) {
+                prevButton.setEnabled(false);
+            } else {
+                prevButton.setEnabled(true);
+            }
+        }
+    }
 }
